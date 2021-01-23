be28b647eb67f1215d48473ac2b17f74f0e04378
Rashawn Raiford <raiford87@gmail.com>
2021-01-23 16:08:42 -0500

    Added sort functionality to table

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,16 +1,51 @@
 import React, { useState, useEffect } from "react";
 import User from "../../components/User/User";
+import SortableHeader from "../../components/SortableHeader/SortableHeader";
 import axios from "axios";
 
 const Home = () => {
   const [userState, setUserState] = useState({ users: [] });
+  const [sortState, setSortState] = useState("ascending");
 
   useEffect(() => {
     axios.get("https://randomuser.me/api/?results=10&nat=us").then((res) => {
-      console.log(res.data);
-      setUserState({ users: res.data.results });
+      setUserState({
+        users: res.data.results.map((user) => {
+          return {
+            name: `${user.name.first} ${user.name.last}`,
+            last: user.name.last,
+            cell: user.cell,
+            email: user.email,
+            medium: user.picture.medium,
+          };
+        }),
+      });
     });
   }, []);
+
+  const sortColumn = (name, sort) => {
+    let sorted = userState.users;
+    if (sort === "ascending") {
+      sorted = userState.users.sort((item1, item2) => {
+        if (item1[name] > item2[name]) return 1;
+
+        if (item1[name] < item2[name]) return -1;
+
+        return 0;
+      });
+    } else if (sort === "descending") {
+      sorted = userState.users.sort((item1, item2) => {
+        if (item1[name] > item2[name]) return -1;
+
+        if (item1[name] < item2[name]) return 11;
+
+        return 0;
+      });
+    }
+
+    setSortState(sort === "ascending" ? "descending" : "ascending");
+    setUserState({ users: sorted });
+  };
 
   return (
     <div className="container">
@@ -19,19 +54,31 @@ const Home = () => {
       </div>
       <div className="row">
         <table className="table table-striped">
-            <thead>
+          <thead>
             <tr>
-                <th></th>
-                <th>Name</th>
-                <th>Phone Number</th>
-                <th>Email</th>
+              <th></th>
+              <SortableHeader
+                name="Name"
+                onClick={sortColumn}
+                sort={sortState}
+              />
+              <SortableHeader
+                name="Cell"
+                onClick={sortColumn}
+                sort={sortState}
+              />
+              <SortableHeader
+                name="Email"
+                onClick={sortColumn}
+                sort={sortState}
+              />
             </tr>
-            </thead>
-            <tbody>
-            {
-                userState.users.map((user, index) => <User user={user} key={index}/>)
-            }
-            </tbody>
+          </thead>
+          <tbody>
+            {userState.users.map((user, index) => (
+              <User user={user} key={index} />
+            ))}
+          </tbody>
         </table>
       </div>
     </div>
