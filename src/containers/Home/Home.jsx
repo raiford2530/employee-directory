d1f1cf2366b3024457bd39cfe9c449c92b51667a
Rashawn Raiford <raiford87@gmail.com>
2021-01-23 14:59:17 -0500

    Added call to random user api to populate table on load

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,13 +1,39 @@
-import React, { useState } from "react";
-import User from '../../components/User/User'
+import React, { useState, useEffect } from "react";
+import User from "../../components/User/User";
+import axios from "axios";
 
-const Home = (props) => {
+const Home = () => {
+  const [userState, setUserState] = useState({ users: [] });
+
+  useEffect(() => {
+    axios.get("https://randomuser.me/api/?results=10&nat=us").then((res) => {
+      console.log(res.data);
+      setUserState({ users: res.data.results });
+    });
+  }, []);
+
   return (
     <div className="container">
       <div className="row">
         <h1 className="text-center py-5">Employee Directory</h1>
       </div>
-      <User />
+      <div className="row">
+        <table className="table table-striped">
+            <thead>
+            <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Phone Number</th>
+                <th>Email</th>
+            </tr>
+            </thead>
+            <tbody>
+            {
+                userState.users.map((user, index) => <User user={user} key={index}/>)
+            }
+            </tbody>
+        </table>
+      </div>
     </div>
   );
 };
